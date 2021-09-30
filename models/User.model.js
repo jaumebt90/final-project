4da2d9497ff7a236d43b9e969f1df64de68e0c02
simplePac <rober_alicante@hotmail.es>
2021-09-30 10:17:32 +0200

    Modificación Modelo User

--- models/User.model.js
+++ models/User.model.js
@@ -5,6 +5,11 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
+  rol: {
+        type: String,
+        enum : ['player','coach'],
+        default: 'player'
+    },
 });
 
 module.exports = model("User", userSchema);
