2f0c2198f9c75f5414a7818068b4ee5d1539e0bf
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-10-01 13:35:21 +0200

    Update club.routes.js

--- routes/club.routes.js
+++ routes/club.routes.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const New = require("../models/News.model")
-
+const New = require("../models/News.model");
 
 //  GET /api/projects -  Retrieves all of the projects
 
+const Player = require("../models/Player.model");
+
+router.get("/club", (req, res, next) => {
+  /* console.log(req); */
+
+  Player.find()
+    .then((allPlayers) => res.json(allPlayers))
+    .catch((err) => res.json(err));
+});
 
 module.exports = router;
