27439ff1a580c58c52c2eee3214107b833091fbd
simplePac <rober_alicante@hotmail.es>
2021-10-04 09:34:22 +0200

    Update news.routes.js

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -25,7 +25,6 @@ router.get("/news/:id", (req, res, next) => {
 
 router.put("/news/:id", (req, res, next) => {
   const newId = req.params.id;
-  console.log("AQUIENTRA", newId);
   if (!mongoose.Types.ObjectId.isValid(newId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
